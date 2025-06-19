a39cf5b44a8d24d8339b4919d83671b442a7ca71
islam0122 <duishobaevislam01@gmail.com>
2025-06-19 23:24:02 +0600

    day_04

---
+++ codewars_practice/WEEK_01_Python/day_04.py
@@ -0,0 +1,26 @@
+"""
+Calculate BMI --> 8kyu
+"""
+# def bmi(weight, height):
+#     value = weight / (height ** 2)
+#     if value <= 18.5:
+#         return "Underweight"
+#     elif value <= 25.0:
+#         return "Normal"
+#     elif value <= 30.0:
+#         return "Overweight"
+#     else:
+#         return "Obese"
+
+"""
+Beginner - Reduce but Grow --> 8kyu
+"""
+# def grow(arr):
+#     res = 0
+#     for i in arr:
+#         res = i * res
+#     return res
+
+"""
+
+"""
--- django/myproject/app/models.py
+++ django/myproject/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    image = models.ImageField(upload_to='car_images')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    year_built = models.DateField()
+    color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
