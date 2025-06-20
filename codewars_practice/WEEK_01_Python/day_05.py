29b2441c236979a687175ef9abad6580e561cc04
islam0122 <duishobaevislam01@gmail.com>
2025-06-20 23:01:25 +0600

    day_05

---
+++ codewars_practice/WEEK_01_Python/day_05.py
@@ -0,0 +1,28 @@
+"""
+Square(n) Sum --> 8kyu
+"""
+# def square_sum(numbers):
+#     res = 0
+#     for num in numbers:
+#         res += num**2
+#     return res
+#
+# print(square_sum([1, 2, 2]))
+
+"""
+Break camelCase --> 6kyu
+"""
+# def solution(s):
+#     result = ""
+#     for char in s:
+#         if char.isupper():
+#             result += " " + char
+#         else:
+#             result += char
+#     return result
+#
+# print(solution("cmmalCammelRose"))
+
+"""
+
+"""
